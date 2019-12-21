c7962b1b8a915107bcb5ea6ad2f68e7e805fbbef
Pavel Maskevich <pavel.maskevich@gmail.com>
2019-12-21 18:51:50 +0300

    Added build method for tree node

--- API/TreeNode.cs
+++ API/TreeNode.cs
@@ -1,7 +1,35 @@
-﻿namespace API
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API
 {
     public class TreeNode
     {
+        public static TreeNode Build(int?[] input)
+        {
+            if (input == null || input.Length == 0 || input[0] == null) return null;
+
+            var nodes = input.Select(x => x == null ? null : new TreeNode(x.Value)).ToList();
+            for (int i = 0; i < nodes.Count(); i++)
+            {
+                var node = nodes[i];
+                if (node == null) continue;
+
+                var j = 2 * i + 1;
+                if (j < nodes.Count)
+                {
+                    node.left = nodes[j++];
+                }
+                if (j < nodes.Count)
+                {
+                    node.right = nodes[j];
+                }
+            }
+
+            return nodes[0];
+        }
+
         public int val;
         public TreeNode left;
         public TreeNode right;
