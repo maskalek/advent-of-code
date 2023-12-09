75310bffffc7bbf4223e006027757c6dac5051a0
Pavel Maskevich <pavel.maskevich@gmail.com>
2023-12-09 09:28:02 +0100

    Implement digit string replacement in GetCalibrationValue
    A new Dictionary called digitMap has been added to the GetCalibrationValue function to handle the replacement of digit strings with a certain pattern. Also, tests have been updated in the UnitTest1.cs file to reflect this change in the calibration method. This modification is intended to standardize the characters included in the processed lines.

--- API/Solution.cs
+++ API/Solution.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public class Solution
 {
@@ -15,6 +16,16 @@ public class Solution
 
     private int GetCalibrationValue(string line)
     {
+        var digitMap = new Dictionary<string, string>()
+        {
+            {"one", "o1e"}, {"two", "t2o"}, {"three", "t3e"}, {"four", "f4r"},
+            {"five", "f5e"}, {"six", "s6x"}, {"seven", "s7n"}, {"eight", "e8t"}, {"nine", "n9e"}
+        };
+
+        foreach (var entry in digitMap)
+        {
+            line = line.Replace(entry.Key, entry.Value);
+        }
         string cleanedLine = new string(line.Where(char.IsDigit).ToArray());
 
         string firstDigit = cleanedLine[0].ToString();
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -23,11 +23,14 @@ namespace Tests
         {
             yield return new object[]
             {
-                142,
-                "1abc2",
-                "pqr3stu8vwx",
-                "a1b2c3d4e5f",
-                "treb7uchet"
+                281,
+                "two1nine", 
+                "eightwothree",
+                "abcone2threexyz", 
+                "xtwone3four", 
+                "4nineeightseven2",
+                "zoneight234", 
+                "7pqrstsixteen"
             };
         }
 
