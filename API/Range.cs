5de046bb27ef45148371268ea7207b9c14f5303e
Pavel Maskevich <pavel.maskevich@gmail.com>
2023-12-10 18:52:12 +0100

    day 5: part 1

---
+++ API/Range.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace API;
+
+public class Transformation
+{
+    private long _start;
+    private long _end;
+    private long _diff;
+
+    public Transformation(string line)
+    {
+        var parts = line.Split(" ").Select(x => long.Parse(x)).ToArray();
+        _start = parts[1];
+        _end = _start + parts[2] - 1;
+        _diff = parts[0] - parts[1];
+    }
+
+    public bool Contains(long x)
+    {
+        if (x >= _start && x <= _end)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public long Transform(long x)
+    {
+        return x + _diff;
+    }
+}
--- API/Solution.cs
+++ API/Solution.cs
@@ -1,7 +1,55 @@
-﻿public class Solution
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using API;
+
+public class Solution
 {
-    public int Sum(string[] lines)
+    public long Sum(string[] lines)
     {
-        return 0;
+        // seeds: 79 14 55 13
+        var seeds = lines[0].Split(":")[1].Trim().Split(" ").Select(x => long.Parse(x.ToString())).ToArray();
+
+        var transformations = new List<Transformation>[7];
+        // seed-to-soil map:
+        // 50 98 2
+        // 52 50 48
+        var k = -1;
+        for (var j = 1; j < lines.Length; j++)
+        {
+            var line = lines[j];
+            if (string.IsNullOrEmpty(line) || !char.IsNumber(line[0]))
+            {
+                k++;
+                j++;
+                transformations[k] = new();
+                continue;
+            }
+            transformations[k].Add(new Transformation(line));
+        }
+        
+        foreach (var t in transformations)
+        {
+            for (var i = 0; i < seeds.Length; i++)
+            {
+                seeds[i] = Calculate(seeds[i], t);
+            }
+        }
+
+        return seeds.Min();
+    }
+
+    private long Calculate(long seed, List<Transformation> transformations)
+    {
+        var res = long.MaxValue;
+        foreach (var t in transformations)
+        {
+            if (t.Contains(seed))
+            {
+                res = Math.Min(res, t.Transform(seed));
+            }
+        }
+
+        return res == long.MaxValue ? seed : res;
     }
 }
